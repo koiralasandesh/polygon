4fb065a128b381cd241958b793347245e11decd5
Sandesh Koirala <sandesh.koirala@mavs.uta.edu>
2020-02-08 16:49:17 -0600

    code cleanup, final code

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,21 @@
 #include "polygon.h"
 
 int main(){
-    std::cout<<"Enter length of side: (negative or 0 to exit)"<<std::endl;
     double length;
     Polygon polygon;
     while(true){
+        std::cout<<"Enter length of side (negative or 0 to exit):"<<std::endl;
         std::cin>>length;
         if (length<=0){
             std::cout<< "Invalid length entered, exiting..."<<std::endl;
-            std::cout<<"Current Polygon perimeter: "<<polygon.get_perimeter()<<"units, sides: "<<polygon.get_sides()<< std::endl;
+            std::cout<<"Current Polygon perimeter: "<<polygon.get_perimeter()<<" units, sides: "<<polygon.get_sides()<<"."<< std::endl;
+            std::cout<<"=========================END================================"<<std::endl;
             break;
         }
         polygon.add_side(length);
-        std::cout<<"Current Polygon perimeter: "<<polygon.get_perimeter()<<"units, sides: "<<polygon.get_sides()<<std::endl;
+        
+        std::cout<<"Current Polygon perimeter: "<<polygon.get_perimeter()<<" units, sides: "<<polygon.get_sides()<<"."<<std::endl;
+        std::cout<<"--------------------------------------------------------------"<<std::endl;
     }
 
 }
